c49c5210aae0bc8b66df2c0c9c46fb843d69f6bd
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-12-17 16:22:49 -0500

    scaffolded project

---
+++ routes/users-model.js
@@ -0,0 +1,23 @@
+const db = require("../data/dbConfig.js");
+
+async function insert(user) {
+  return null;
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
+
+function getUsers() {
+  return null;
+}
+
+function getById(id) {
+  return null;
+}
+
+module.exports = { insert, update, remove, getUsers, getById };
